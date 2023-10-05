1798458ef0352029b2d486ec6ded664d68be0569
Karl Vinuya <karl_vinuya@yahoo.ca>
2023-10-04 18:44:13 -0600

    Revert "Add private vars to Shape"
    This reverts commit 4e8b12636db8b6eebd0e2b79b8a3186b2975ef28.

--- app/src/main/java/com/example/cmputlab4/Shape.java
+++ app/src/main/java/com/example/cmputlab4/Shape.java
@@ -1,8 +1,8 @@
 package com.example.cmputlab4;
 
 public abstract class Shape {
-    private Integer x;
-    private Integer y;
+    Integer x;
+    Integer y;
 
     public Shape(Integer x, Integer y) {
         this.x = x;
