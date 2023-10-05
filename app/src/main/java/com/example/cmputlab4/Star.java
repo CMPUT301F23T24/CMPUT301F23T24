882269ef09fd20611777a4d464c61091a9b11e0a
Cohen <ccalver1@ualberta.ca>
2023-10-04 18:58:28 -0600

    added colour blue

--- app/src/main/java/com/example/cmputlab4/Star.java
+++ app/src/main/java/com/example/cmputlab4/Star.java
@@ -5,5 +5,15 @@ public class Star extends Shape{
     public Star(Integer x, Integer y) {
         super(x, y);
     }
-    
+
+    @Override
+    public double area(){
+        // Do nothing
+        return 0;
+    }
+    @Override
+    public double perimeter(){
+        // Do nothing
+        return 0;
+    }
 }
