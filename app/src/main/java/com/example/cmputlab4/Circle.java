8bec5b6304982020e9ae12ab246f4ed0e0abbe5d
Silin Song <silin5@ualberta.ca>
2023-10-04 18:48:34 -0600

    feat: Circle class

--- app/src/main/java/com/example/cmputlab4/Circle.java
+++ app/src/main/java/com/example/cmputlab4/Circle.java
@@ -1,2 +1,28 @@
-package com.example.cmputlab4;public class Circle {
+package com.example.cmputlab4;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(int x, int y, double radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
 }
