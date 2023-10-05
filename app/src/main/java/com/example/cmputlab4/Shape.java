4cffe4008228c3c16008f8d748861415307cdecb
Moon <145698097+CohenCalvert@users.noreply.github.com>
2023-10-04 19:02:26 -0600

    Update Shape.java

--- app/src/main/java/com/example/cmputlab4/Shape.java
+++ app/src/main/java/com/example/cmputlab4/Shape.java
@@ -3,7 +3,7 @@ package com.example.cmputlab4;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-
+    String color = "green";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
