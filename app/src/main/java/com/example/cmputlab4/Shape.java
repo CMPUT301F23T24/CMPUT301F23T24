c4bb2350a46464c84368215a0c6fc8b0648b90cc
Ajaggi24 <vkgagret@gmail.com>
2023-10-07 22:33:32 -0600

    changes made in shape.java

--- app/src/main/java/com/example/cmputlab4/Shape.java
+++ app/src/main/java/com/example/cmputlab4/Shape.java
@@ -4,7 +4,6 @@ public abstract class Shape {
     private Integer x;
     private Integer y;
     String color = "blue";
-
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
