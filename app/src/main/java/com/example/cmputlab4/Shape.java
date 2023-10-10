3fb6362d86f92624b53c6e3692fa512677f014d6
Anurag Chopra <anurag23.chopra@gmail.com>
2023-10-10 11:48:23 -0600

    updated shape.java

--- app/src/main/java/com/example/cmputlab4/Shape.java
+++ app/src/main/java/com/example/cmputlab4/Shape.java
@@ -3,11 +3,7 @@ package com.example.cmputlab4;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-<<<<<<< HEAD
-    String color = "blue";
-=======
     String color = "red";
->>>>>>> 88dd0fd4925c7085a6e504a4b32633ca9f833b4e
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
